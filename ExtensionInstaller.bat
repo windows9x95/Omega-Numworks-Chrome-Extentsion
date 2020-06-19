d84e395799ac5f825e5cafb8151c3ef4a348168e
Juju <59638234+windows9x95@users.noreply.github.com>
2020-06-19 15:02:29 +0200

    added another path for Google Chrome
    If Chrome was installed in C:\Programs Files (x86)\Google\Chrome it wasn't working but I added that path in the file

--- ExtensionInstaller.bat
+++ ExtensionInstaller.bat
@@ -15,6 +15,17 @@ if exist %userprofile%\AppData\Local\Chrome  (
      %userprofile%\AppData\Local\Chrome\Application\chrome.exe --user-data-dir=%temp% --load-extension=%userprofile%\AppData\Local\Chrome\Application\Omega
      echo Sucessfully Installed the extension to Google Chrome
 )
+if exist %ProgramFiles(x86)%\Google\Chrome  (
+     echo Chrome detected, Installing Omega Extension to Chrome
+     mkdir %ProgramFiles(x86)%\Google\Chrome\Application\Omega
+     mkdir %ProgramFiles(x86)%\Google\Chrome\Application\Omega\img
+     copy img\Omega-Logo.png %ProgramFiles(x86)%\Google\Chrome\Application\Omega\img\Omega-Logo.png
+     copy manifest.json %ProgramFiles(x86)%\Google\Chrome\Application\Omega\manifest.json
+     copy popup.html %ProgramFiles(x86)%\Google\Chrome\Application\Omega\popup.html
+     copy style.css %ProgramFiles(x86)%\Google\Chrome\Application\Omega\style.css
+     %ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe --user-data-dir=%temp% --load-extension=%ProgramFiles(x86)%\Google\Chrome\Application\Omega
+     echo Sucessfully Installed the extension to Google Chrome
+)
 if exist %userprofile%\AppData\Local\Chromium  (
      echo Chromium detected, Installing Omega Extension to Chromium
      mkdir %userprofile%\AppData\Local\Chromium\Application\Omega
